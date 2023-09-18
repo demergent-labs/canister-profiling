de73fc7c041640bb76d1b8b2e9a290b49d33528b
Dan Steren <dan@demergentlabs.org>
2023-09-18 14:58:32 -0600

    Add Azle implementations of Heartbeat and Timer canisters

--- heartbeat/perf.sh
+++ heartbeat/perf.sh
@@ -5,6 +5,8 @@ let heartbeat_mo = wasm_profiling("motoko/.dfx/local/canisters/heartbeat/heartbe
 let timer_mo = wasm_profiling("motoko/.dfx/local/canisters/timer/timer.wasm");
 let heartbeat_rs = wasm_profiling("rust/.dfx/local/canisters/heartbeat/heartbeat.wasm");
 let timer_rs = wasm_profiling("rust/.dfx/local/canisters/timer/timer.wasm");
+let heartbeat_ts = wasm_profiling("azle/.dfx/local/canisters/heartbeat/heartbeat.wasm");
+let timer_ts = wasm_profiling("azle/.dfx/local/canisters/timer/timer.wasm");
 
 let file = "README.md";
 
@@ -18,9 +20,9 @@ function heartbeat_perf(wasm, title) {
 function timer_perf(wasm, title) {
   let cid = install(wasm, encode (), null);
   output(file, stringify("|", title, "|", wasm.size(), "|"));
-  
+
   call cid.__toggle_entry();
-  let tid = call cid.setTimer(0);
+  let tid = call cid.setTimer((0: nat));
   // A second update call can usually capture both the job and setTimer, but it is flaky.
   call cid.__toggle_entry();
   let svg = stringify(title, "_setTimer.svg");
@@ -28,7 +30,7 @@ function timer_perf(wasm, title) {
   output(file, stringify("[", cost, "](", svg, ")|"));
 
   //call cid.__toggle_entry();
-  let tid = call cid.setTimer(10);
+  let tid = call cid.setTimer((10: nat));
   call cid.cancelTimer(tid);
   let svg = stringify(title, "_cancelTimer.svg");
   flamegraph(cid, stringify(title, ".cancelTimer"), svg);
@@ -39,7 +41,9 @@ function timer_perf(wasm, title) {
 output(file, "\n## Heartbeat\n\n| |binary_size|heartbeat|\n|--:|--:|--:|\n");
 heartbeat_perf(heartbeat_mo, "Motoko");
 heartbeat_perf(heartbeat_rs, "Rust");
+heartbeat_perf(heartbeat_ts, "Azle");
 
 output(file, "\n## Timer\n\n| |binary_size|setTimer|cancelTimer|\n|--:|--:|--:|--:|\n");
 timer_perf(timer_mo, "Motoko");
 timer_perf(timer_rs, "Rust");
+timer_perf(timer_ts, "Azle");
